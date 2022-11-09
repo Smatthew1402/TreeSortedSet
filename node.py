c282933ee9c453381b46a5a92e6700fd634ea63c
Smatthew1402 <100808047+Smatthew1402@users.noreply.github.com>
2022-11-08 21:46:26 -0500

    Added parent variable

--- node.py
+++ node.py
@@ -1,8 +1,9 @@
 class Node:
-    def __init__(self, value, left = None, right = None):
+    def __init__(self, value, left = None, right = None, parent = None):
         self.data = value
         self.left = left
         self.right = right
+        self.parent = parent
     
     def __repr__(self):
         return "(" + str(self.data) + " " + str(self.left) + " " + str(self.right) + ")"
